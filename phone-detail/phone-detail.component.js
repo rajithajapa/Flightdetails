f15b1def0a6c66af84b80a6ef07cf6944af27110
rajithajapa <japa.rajithareddy@gmail.com>
2016-08-30 15:32:04 -0700

    first commit

---
+++ phone-detail/phone-detail.component.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Register `phoneDetail` component, along with its associated controller and template
+angular.
+  module('phoneDetail').
+  component('phoneDetail', {
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$routeParams', 'Phone',
+      function PhoneDetailController($routeParams, Phone) {
+        var self = this;
+        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        });
+
+      }
+    ]
+  });
